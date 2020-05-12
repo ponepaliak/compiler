83e4e1e488001f2852aeb338d8306e3a7d24dd7f
Andrii <ponepaliak@gmail.com>
2020-05-12 16:05:47 +0300

    Change file path

--- simple-compiler/Program.cs
+++ simple-compiler/Program.cs
@@ -11,7 +11,7 @@ namespace simple_compiler
             ErrorPointer errorPointer = new ErrorPointer();
             TextReaderFromFile textReaderFromFile = new TextReaderFromFile(errorPointer);
             errorPointer.SetTextReader(textReaderFromFile);
-            textReaderFromFile.SetPath("E:\\Rider\\simple-compiler\\simple-compiler\\code-example.txt");
+            textReaderFromFile.SetPath("..\\..\\code-example.txt");
             IScanner scanner = new Scanner(textReaderFromFile, errorPointer);
             scanner.NextToken();
             Table table = new Table(errorPointer);
